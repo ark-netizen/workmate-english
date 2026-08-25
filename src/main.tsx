eb8bb0958d878e857a82efe7e9448539a48a89c0
sua <ark230015@gmail.com>
2026-08-25 20:47:31 +0900

    모바일 체험 직급 아바타 스타일 연결

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./intro-mobile-hotfix.css";
 import "./mobile-responsive-v2.css";
 import "./mobile-trial-preview.css";
 import "./mobile-trial-preview-v2.css";
+import "./mobile-trial-fidelity.css";
 import "./enable-mobile-intro-trial";
 
 createRoot(document.getElementById("root")!).render(
