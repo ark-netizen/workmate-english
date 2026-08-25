1e82e3eb5a11f925ab4aad86e0bbb387002ed624
sua <ark230015@gmail.com>
2026-08-26 01:38:29 +0900

    비즈니스 WORK PROCESS 배분 정렬 CSS 연결

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import "./mobile-rank-otters.css";
 import "./intro-nav-responsive.css";
 import "./intro-scroll-enhance.css";
 import "./intro-work-process-stream.css";
+import "./intro-business-process-layout.css";
 import "./intro-scroll-enhance";
 import "./intro-work-process-stream";
 import "./intro-copy-polish";
