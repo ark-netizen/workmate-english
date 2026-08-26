96e0cba5e6e71764429d861d3b6cea2566d6f213
sua <ark230015@gmail.com>
2026-08-26 14:14:18 +0900

    게임 WORK PROCESS 여백 보정 CSS 연결

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import "./mobile-rank-otters.css";
 import "./intro-nav-responsive.css";
 import "./intro-scroll-enhance.css";
 import "./intro-business-process-layout.css";
+import "./intro-game-process-layout.css";
 import "./intro-game-flutter.css";
 import "./intro-scroll-enhance";
 import "./intro-game-flutter";
